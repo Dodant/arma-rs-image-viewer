22c8df2b2843098c7f1a9f858bc50337e505f3b4
Dodant <ohho0506@gmail.com>
2023-02-09 14:07:53 +0900

    imple : bbox (only logic)

--- imageplot.py
+++ imageplot.py
@@ -16,14 +16,15 @@ anno_file = pd.read_csv(anno_filepath)
 cv2.startWindowThread()
 
 for _, row in anno_file.iterrows():
+    ## centered point
     # center_x, center_y = int(row['center_x']), int(row['center_y'])
     # cv2.circle(canvas, (center_x, center_y), 1, (0,0,255), 2)
-    print(list(map(int, row['x1':'y4'])))
-    pts = list(row['x1'], row['y1']), list(row['x2'], row['y2']), list(row['x3'], row['y3']), int(row['x4'], row['y4'])
-    polygon = np.array([pts], dtype=np.int32)
-    cv2.polylines(canvas, [polygon], True, (0, 0, 255), 2)
 
-# cv2.imshow('canvas', canvas)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-# cv2.waitKey(1)
+    ## bbox
+    # pts = list(map(list, [row['x1':'y1'], row['x2':'y2'], row['x3':'y3'], row['x4':'y4']]))
+    # polygon = np.array([pts], dtype=np.int32)
+    # cv2.polylines(canvas, [polygon], True, (0, 0, 255), 1)
+
+cv2.imshow('canvas', canvas)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
