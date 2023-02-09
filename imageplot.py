6ffc402d1ae1f70155ad0b647089467c71c3fa85
Dodant <ohho0506@gmail.com>
2023-02-09 15:29:24 +0900

    imple : label (only logic)

--- imageplot.py
+++ imageplot.py
@@ -4,6 +4,9 @@ from PIL import Image
 import numpy as np
 import pandas as pd
 
+import warnings
+warnings.filterwarnings('ignore')
+
 eo_filepath = '/home/dodant/Downloads/malden-sunny-10-08/00000.classes_W.csv.result/IMG/EO.png'
 ir_filepath = '/home/dodant/Downloads/malden-sunny-10-08/00000.classes_W.csv.result/IMG/IR.png'
 anno_filepath = '/home/dodant/Downloads/malden-sunny-10-08/00000.classes_W.csv.result/annotations.csv'
@@ -24,6 +27,10 @@ for _, row in anno_file.iterrows():
     # pts = list(map(list, [row['x1':'y1'], row['x2':'y2'], row['x3':'y3'], row['x4':'y4']]))
     # polygon = np.array([pts], dtype=np.int32)
     # cv2.polylines(canvas, [polygon], True, (0, 0, 255), 1)
+
+    ## label
+    center_x, center_y = int(row['center_x']), int(row['center_y'])
+    cv2.putText(canvas, row['sub_class'], (center_x+5, center_y+5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
 
 cv2.imshow('canvas', canvas)
 cv2.waitKey(0)
