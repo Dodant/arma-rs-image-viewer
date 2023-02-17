007667287fb2bff02b61db0e68cd8480fe8b4072
Dodant <ohho0506@gmail.com>
2023-02-17 13:32:17 +0900

    [Refactor]

--- armaviewer.py
+++ armaviewer.py
@@ -37,6 +37,7 @@ class ArmaViewer(QWidget):
         self.checked: list = []
         self.anno_file = None
         self.label_color = {}
+        self.currentOS = platform.system()
 
         self.folderlabel = QLabel(f'Folder Name : {self.fname}', self)
         self.folderImagePairNumLabel = QLabel('Image PAIR: _ | EO: _ | IR: _')
@@ -98,8 +99,10 @@ class ArmaViewer(QWidget):
         self.selected = self.imgType = self.fileExtractor('img_type')
         self.changeImageAtAllOnce()
 
-        if self.imgType == 'EO': self.eo_radiobtn.setChecked(True)
-        elif self.imgType == 'IR': self.ir_radiobtn.setChecked(True)
+        if self.imgType == 'EO':
+            self.eo_radiobtn.setChecked(True)
+        elif self.imgType == 'IR':
+            self.ir_radiobtn.setChecked(True)
 
     def changeImageAtAllOnce(self):
         self.checkboxToggle()
@@ -116,8 +119,58 @@ class ArmaViewer(QWidget):
 
     def plot(self):
 
+        def plotCanvas(canvas):
+
+            def plotCenteredPtsImage(canvas_):
+                self.anno_file = pd.read_csv(self.fileExtractor('annotation_path'))
+                for _, row_ in self.anno_file.iterrows():
+                    center = list(map(int, row_['center_x':'center_y']))
+                    cv2.circle(canvas_, center, 1, self.label_color[f'{row_["main_class"]}-{row_["middle_class"]}'], 2)
+                return canvas_
+
+            def plotBboxImage(canvas_):
+                self.anno_file = pd.read_csv(self.fileExtractor('annotation_path'))
+                for _, row_ in self.anno_file.iterrows():
+                    pts = list(map(list, [row_['x1':'y1'], row_['x2':'y2'], row_['x3':'y3'], row_['x4':'y4']]))
+                    polygon = np.array([pts], dtype=np.int32)
+                    cv2.polylines(canvas_, [polygon], True,
+                                  self.label_color[f'{row_["main_class"]}-{row_["middle_class"]}'], 2)
+                return canvas_
+
+            def plotRboxImage(canvas_):
+
+                def dist(center_, point):
+                    a, b = center_[0] - point[0], center_[1] - point[1]
+                    return int(math.sqrt(a ** 2 + b ** 2))
+
+                self.anno_file = pd.read_csv(self.fileExtractor('annotation_path'))
+                for _, row_ in self.anno_file.iterrows():
+                    center = list(map(int, row_['center_x':'center_y']))
+                    cv2.circle(canvas_, center, dist(center, row_['x1':'y1']),
+                               self.label_color[f'{row_["main_class"]}-{row_["middle_class"]}'], 2)
+                return canvas_
+
+            def plotLabelImage(canvas_):
+                self.anno_file = pd.read_csv(self.fileExtractor('annotation_path'))
+                for _, row_ in self.anno_file.iterrows():
+                    center = list(map(int, row_['center_x':'center_y']))
+                    cv2.putText(canvas_, f'{row_["main_class"]}-{row_["middle_class"]}', (center[0] + 5, center[1] + 5),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.6,
+                                self.label_color[f'{row_["main_class"]}-{row_["middle_class"]}'], 2)
+                return canvas_
+
+            self.anno_file = pd.read_csv(self.fileExtractor('annotation_path'))
+            for _, row in self.anno_file.iterrows():
+                self.label_color[f'{row["main_class"]}-{row["middle_class"]}'] = \
+                    (random.randint(128, 255), random.randint(128, 255), random.randint(128, 255))
+            if 0 in self.checked: canvas = plotCenteredPtsImage(canvas)
+            if 1 in self.checked: canvas = plotBboxImage(canvas)
+            if 2 in self.checked: canvas = plotRboxImage(canvas)
+            if 3 in self.checked: canvas = plotLabelImage(canvas)
+            return canvas
+
         def setScaleAndPolicy(canvas):
-            canvas = self.plotCanvas(canvas)
+            canvas = plotCanvas(canvas)
             self.pixmap = QPixmap(q2n.array2qimage(canvas, normalize=False))
             self.lbl_img.setPixmap(self.pixmap)
             self.lbl_img.setScaledContents(True)
@@ -134,80 +187,32 @@ class ArmaViewer(QWidget):
         if self.selected == 'IR':
             setScaleAndPolicy(cv2.cvtColor(cv2.imread(self.fileExtractor('ir_full_path')), cv2.COLOR_BGR2RGB))
 
-    def plotCanvas(self, canvas):
-
-        def plotCenteredPtsImage(canvas_):
-            self.anno_file = pd.read_csv(self.fileExtractor('annotation_path'))
-            for _, row_ in self.anno_file.iterrows():
-                center = list(map(int, row_['center_x':'center_y']))
-                cv2.circle(canvas_, center, 1, self.label_color[f'{row_["main_class"]}-{row_["middle_class"]}'], 2)
-            return canvas_
-
-        def plotBboxImage(canvas_):
-            self.anno_file = pd.read_csv(self.fileExtractor('annotation_path'))
-            for _, row_ in self.anno_file.iterrows():
-                pts = list(map(list, [row_['x1':'y1'], row_['x2':'y2'], row_['x3':'y3'], row_['x4':'y4']]))
-                polygon = np.array([pts], dtype=np.int32)
-                cv2.polylines(canvas_, [polygon], True, self.label_color[f'{row_["main_class"]}-{row_["middle_class"]}'], 2)
-            return canvas_
-
-        def plotRboxImage(canvas_):
-
-            def dist(center_, point):
-                a, b = center_[0] - point[0], center_[1] - point[1]
-                return int(math.sqrt(a ** 2 + b ** 2))
-
-            self.anno_file = pd.read_csv(self.fileExtractor('annotation_path'))
-            for _, row_ in self.anno_file.iterrows():
-                center = list(map(int, row_['center_x':'center_y']))
-                cv2.circle(canvas_, center, dist(center, row_['x1':'y1']), self.label_color[f'{row_["main_class"]}-{row_["middle_class"]}'], 2)
-            return canvas_
-
-        def plotLabelImage(canvas_):
-            self.anno_file = pd.read_csv(self.fileExtractor('annotation_path'))
-            for _, row_ in self.anno_file.iterrows():
-                center = list(map(int, row_['center_x':'center_y']))
-                cv2.putText(canvas_, f'{row_["main_class"]}-{row_["middle_class"]}', (center[0] + 5, center[1] + 5),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.6,
-                            self.label_color[f'{row_["main_class"]}-{row_["middle_class"]}'], 2)
-            return canvas_
-
-        self.anno_file = pd.read_csv(self.fileExtractor('annotation_path'))
-        for _, row in self.anno_file.iterrows():
-            self.label_color[f'{row["main_class"]}-{row["middle_class"]}'] = \
-                (random.randint(128, 255), random.randint(128, 255), random.randint(128, 255))
-        if 0 in self.checked: canvas = plotCenteredPtsImage(canvas)
-        if 1 in self.checked: canvas = plotBboxImage(canvas)
-        if 2 in self.checked: canvas = plotRboxImage(canvas)
-        if 3 in self.checked: canvas = plotLabelImage(canvas)
-        return canvas
-
     def fileExtractor(self, case: str):
         # /home/dodant/Downloads/malden-sunny-10-08 (Sample Data)/00000.classes_W.csv.result/IMG/EO.png
         if case == 'eo_full_path':
-            if platform.system() == 'Linux': return pth.join(pth.dirname(self.fname), 'EO.png')
-            if platform.system() == 'Windows': return pth.dirname(self.fname) + '/EO.png'
+            if self.currentOS == 'Linux': return pth.join(pth.dirname(self.fname), 'EO.png')
+            if self.currentOS == 'Windows': return pth.dirname(self.fname) + '/EO.png'
         # /home/dodant/Downloads/malden-sunny-10-08 (Sample Data)/00000.classes_W.csv.result/IMG/IR.png
         if case == 'ir_full_path':
-            if platform.system() == 'Linux': return pth.join(pth.dirname(self.fname), 'IR.png')
-            if platform.system() == 'Windows': return pth.dirname(self.fname) + '/IR.png'
+            if self.currentOS == 'Linux': return pth.join(pth.dirname(self.fname), 'IR.png')
+            if self.currentOS == 'Windows': return pth.dirname(self.fname) + '/IR.png'
         # /home/dodant/Downloads/malden-sunny-10-08 (Sample Data)/00000.classes_W.csv.result/annotation.csv
         if case == 'annotation_path':
-            if platform.system() == 'Linux': return f'{pth.sep.join(self.fname.split(pth.sep)[:-2])}{pth.sep}annotations.csv'
-            if platform.system() == 'Windows': return '\\'.join(self.fname.split('/')[:-2]) + '\\' + 'annotations.csv'
+            if self.currentOS == 'Linux': return f'{pth.sep.join(self.fname.split(pth.sep)[:-2])}{pth.sep}annotations.csv'
+            if self.currentOS == 'Windows': return '\\'.join(self.fname.split('/')[:-2]) + '\\' + 'annotations.csv'
         # malden-sunny-10-08 (Sample Data)
         if case == 'folder_name': return pth.basename(pth.dirname(pth.dirname(pth.dirname(self.fname))))
         # /home/dodant/Downloads/malden-sunny-10-08 (Sample Data)
         if case == 'folder_path':
-            if platform.system() == 'Linux': return pth.sep.join(self.fname.split(pth.sep)[:-3])
-            if platform.system() == 'Windows': return '/'.join(self.fname.split('/')[:-3])
+            if self.currentOS == 'Linux': return pth.sep.join(self.fname.split(pth.sep)[:-3])
+            if self.currentOS == 'Windows': return '/'.join(self.fname.split('/')[:-3])
         # 00000.classes_W.csv.result
         if case == 'image_name': return pth.basename(pth.dirname(pth.dirname(self.fname)))
         # EO
         if case == 'img_type': return self.fname[-6:-4]
         if case == 'now_index':
-            if platform.system() == 'Linux': return self.fileLists.index(pth.sep.join(self.fname.split(pth.sep)[:-2]))
-            if platform.system() == 'Windows': return self.fileLists.index('\\'.join(self.fname.split('/')[:-2]))
+            if self.currentOS == 'Linux': return self.fileLists.index(pth.sep.join(self.fname.split(pth.sep)[:-2]))
+            if self.currentOS == 'Windows': return self.fileLists.index('\\'.join(self.fname.split('/')[:-2]))
 
     def btnClicked(self, sign):
         for button in self.btnGroup.buttons():
@@ -222,12 +227,6 @@ class ArmaViewer(QWidget):
 
     def changeImageInfo(self):
         self.fileNumName.setText(f'#{self.fileExtractor("now_index")} | File Name : {self.fileExtractor("image_name")}')
-
-    def imageMix(self):
-        random.shuffle(self.fileLists)
-
-    def imageSorted(self):
-        self.fileLists.sort()
 
     def goToPrevImage(self):
         self.nowIndex -= 1
@@ -252,7 +251,7 @@ class ArmaViewer(QWidget):
         msgBox = QMessageBox()
         msgBox.setWindowTitle("Hello Out There")
         msgBox.setTextFormat(Qt.RichText)
-        msg = "¯\_(ツ)_/¯ \
+        msg = "¯\\_(ツ)_/¯ \
             <br> Copyright (c) 2023 Junggyun Oh. All rights reserved. \
             <br> Please Report Bug and Additional Requirements Here. And Give Me Star. \
             <br> => <a href='https://github.com/Dodant/arma-rs-image-viewer'>Dodant/arma-rs-image-viewer</a>"
@@ -267,38 +266,38 @@ class ArmaViewer(QWidget):
 
         fhbox = QHBoxLayout()
         fhbox.addStretch(1)
-        fhbox.addWidget(folderSelectBtn)
-        fhbox.addWidget(self.folderlabel)
+        fhbox.addWidget(folderSelectBtn, alignment=Qt.AlignCenter)
+        fhbox.addWidget(self.folderlabel, alignment=Qt.AlignCenter)
         fhbox.addStretch(1)
 
         imageMixBtn = QPushButton('Shuffle Images', self)
         imageSortBtn = QPushButton('Sort Images', self)
-        imageMixBtn.clicked.connect(self.imageMix)
-        imageSortBtn.clicked.connect(self.imageSorted)
+        imageMixBtn.clicked.connect(lambda: random.shuffle(self.fileLists))
+        imageSortBtn.clicked.connect(lambda: self.fileLists.sort())
 
         imageMixSortBox = QHBoxLayout()
         imageMixSortBox.addStretch(1)
-        imageMixSortBox.addWidget(self.folderImagePairNumLabel)
-        imageMixSortBox.addWidget(imageMixBtn)
-        imageMixSortBox.addWidget(imageSortBtn)
+        imageMixSortBox.addWidget(self.folderImagePairNumLabel, alignment=Qt.AlignCenter)
+        imageMixSortBox.addWidget(imageMixBtn, alignment=Qt.AlignCenter)
+        imageMixSortBox.addWidget(imageSortBtn, alignment=Qt.AlignCenter)
         imageMixSortBox.addStretch(1)
 
-        prevBtn = QPushButton('<< <<< <', self)
-        nextBtn = QPushButton('> >>> >>', self)
+        prevBtn = QPushButton('<<< << <', self)
+        nextBtn = QPushButton('> >> >>>', self)
         prevBtn.clicked.connect(self.goToPrevImage)
         nextBtn.clicked.connect(self.goToNextImage)
 
         prenextBox = QHBoxLayout()
         prenextBox.addStretch(1)
-        prenextBox.addWidget(prevBtn)
-        prenextBox.addWidget(self.fileNumName)
-        prenextBox.addWidget(nextBtn)
+        prenextBox.addWidget(prevBtn, alignment=Qt.AlignCenter)
+        prenextBox.addWidget(self.fileNumName, alignment=Qt.AlignCenter)
+        prenextBox.addWidget(nextBtn, alignment=Qt.AlignCenter)
         prenextBox.addStretch(1)
 
         hbox = QHBoxLayout()
         hbox.addStretch(1)
-        hbox.addWidget(self.createImageGroup())
-        hbox.addWidget(self.createLabelGroup())
+        hbox.addWidget(self.createImageGroup(), alignment=Qt.AlignCenter)
+        hbox.addWidget(self.createLabelGroup(), alignment=Qt.AlignCenter)
         hbox.addStretch(1)
 
         reportBtn = QPushButton('Report Issue', self)
@@ -308,8 +307,8 @@ class ArmaViewer(QWidget):
 
         hhbox = QHBoxLayout()
         hhbox.addStretch(1)
-        hhbox.addWidget(reportBtn)
-        hhbox.addWidget(extraBtn)
+        hhbox.addWidget(reportBtn, alignment=Qt.AlignCenter)
+        hhbox.addWidget(extraBtn, alignment=Qt.AlignCenter)
         hhbox.addStretch(1)
 
         vbox = QVBoxLayout()
@@ -317,7 +316,7 @@ class ArmaViewer(QWidget):
         vbox.addLayout(imageMixSortBox)
         vbox.addLayout(prenextBox)
         vbox.addLayout(hbox)
-        vbox.addWidget(self.lbl_img)
+        vbox.addWidget(self.lbl_img, alignment=Qt.AlignCenter)
         vbox.addLayout(hhbox)
 
         self.setLayout(vbox)
@@ -328,19 +327,19 @@ class ArmaViewer(QWidget):
 
     def createImageGroup(self):
         hbox = QHBoxLayout()
-        hbox.addWidget(self.eo_radiobtn)
-        hbox.addWidget(self.ir_radiobtn)
-        hbox.addWidget(self.eoir_radiobtn)
+        hbox.addWidget(self.eo_radiobtn, alignment=Qt.AlignCenter)
+        hbox.addWidget(self.ir_radiobtn, alignment=Qt.AlignCenter)
+        hbox.addWidget(self.eoir_radiobtn, alignment=Qt.AlignCenter)
         groupbox = QGroupBox('Image Setting')
         groupbox.setLayout(hbox)
         return groupbox
 
     def createLabelGroup(self):
         hbox = QHBoxLayout()
-        hbox.addWidget(self.center_checkbtn)
-        hbox.addWidget(self.bbox_checkbtn)
-        hbox.addWidget(self.rbox_checkbtn)
-        hbox.addWidget(self.label_checkbtn)
+        hbox.addWidget(self.center_checkbtn, alignment=Qt.AlignCenter)
+        hbox.addWidget(self.bbox_checkbtn, alignment=Qt.AlignCenter)
+        hbox.addWidget(self.rbox_checkbtn, alignment=Qt.AlignCenter)
+        hbox.addWidget(self.label_checkbtn, alignment=Qt.AlignCenter)
         groupbox = QGroupBox('Label Setting')
         groupbox.setLayout(hbox)
         return groupbox
@@ -364,6 +363,7 @@ class ArmaViewer(QWidget):
             self.fname = self.fileExtractor('ir_full_path')
             self.changeImageAtAllOnce()
             self.ir_radiobtn.setChecked(True)
+
 
 if __name__ == '__main__':
     viewer = QApplication(sys.argv)
